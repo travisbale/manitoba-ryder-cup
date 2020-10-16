4968f552d945055f40404f35bc2c51eae4913546
travis <travis.bale@gmail.com>
2020-10-16 03:34:19 -0500

    Add a router-link to the ScoreBar component
    The link will return the user back to the leaderboard.

--- src/components/ScoreBar.vue
+++ src/components/ScoreBar.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="flex align-center justify-between sticky top-0 bg-blue-grey-900 text-grey-200">
-    <div class="w-1/2 flex items-center px-4 py-2 bg-gradient-to-r from-blue-900">
-      <div class="mr-4 text-4xl font-semibold">
-        4.5
+  <router-link :to="{ name: 'leaderboard' }" class="block sticky top-0">
+    <div class="flex align-center justify-between bg-blue-grey-900 text-grey-200">
+      <div class="w-1/2 flex items-center px-4 py-2 bg-gradient-to-r from-blue-900">
+        <div class="mr-4 text-4xl font-semibold">
+          4.5
+        </div>
+        <div class="text-lg pt-1">
+          Sefton
+        </div>
       </div>
-      <div class="text-lg pt-1">
-        Sefton
+      <div class="w-1/2 flex items-center justify-end px-4 py-2 bg-gradient-to-l from-red-900">
+        <div class="mr-4 pt-1 text-lg">
+          Rabe
+        </div>
+        <div class="text-4xl font-semibold">
+          6.5
+        </div>
       </div>
     </div>
-    <div class="w-1/2 flex items-center justify-end px-4 py-2 bg-gradient-to-l from-red-900">
-      <div class="mr-4 pt-1 text-lg">
-        Rabe
-      </div>
-      <div class="text-4xl font-semibold">
-        6.5
-      </div>
-    </div>
-  </div>
+  </router-link>
 </template>
 
 <script>

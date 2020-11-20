e191a2fc68d044c82e4d6340b59e74b8ed569904
travis <travis.bale@gmail.com>
2020-11-20 00:57:13 -0600

    Remove height attribute on the base page
    It was causing issues with the sticky score bar. If the height needs to
    be set to the height of the screen in the future use min height instead.

--- src/components/layout/BasePage.vue
+++ src/components/layout/BasePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex flex-col">
+  <div class="flex flex-col">
     <site-navigation />
     <score-bar v-if="tournamentId > 0" :tournament-id="tournamentId" />
     <div class="lg:container lg:mx-auto">

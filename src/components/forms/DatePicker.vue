6939ed4a85ff2668775799c1e0fe2071c90d4bd6
Travis Bale <travis.bale@gmail.com>
2021-04-13 01:07:39 -0500

    Use v-model to bind the datepicker

--- src/components/forms/DatePicker.vue
+++ src/components/forms/DatePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <datepicker :value="jsDate" input-class="block mb-3 border border-grey-400 rounded-sm px-3 py-2 w-full shadow-sm focus:outline-none" />
+  <datepicker v-model="jsDate" input-class="block mb-3 border border-grey-400 rounded-sm px-3 py-2 w-full shadow-sm focus:outline-none" />
 </template>
 
 <script>
@@ -17,8 +17,13 @@ export default {
   },
 
   computed: {
-    jsDate() {
-      return this.value.toJSDate();
+    jsDate: {
+      get() {
+        return this.value.toJSDate();
+      },
+      set(value) {
+        this.$emit('input', DateTime.fromJSDate(value));
+      },
     },
   },
 };

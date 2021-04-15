b29fb1481ade76940482d5fa6fda7dc829f81b94
Travis Bale <travis.bale@gmail.com>
2021-04-15 00:52:45 -0500

    Adjust size of floating action button

--- src/components/buttons/FloatingActionButton.vue
+++ src/components/buttons/FloatingActionButton.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="p-4 rounded-full bg-indigo-800 hover:bg-indigo-900 shadow-lg fixed bottom-0 right-0 m-8 text-white">
-    <add-icon v-if="action === 'add'" class="w-10 h-10 overflow-hidden text-white" v-on="$listeners" />
-    <div v-if="action === 'edit'" class="w-10 h-10 flex justify-center items-center">
-      <edit-icon class="overflow-hidden text-white" v-on="$listeners" />
-    </div>
+  <div class="p-4 rounded-full bg-indigo-800 hover:bg-indigo-900 shadow-lg fixed bottom-0 right-0 m-4 text-white">
+    <add-icon v-if="action === 'add'" class="overflow-hidden text-white" v-on="$listeners" />
+    <edit-icon v-if="action === 'edit'" class="overflow-hidden text-white" v-on="$listeners" />
   </div>
 </template>
 

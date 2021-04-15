401e1deb5ce3c6883205dcf31c00bcf7836f0e2d
Travis Bale <travis.bale@gmail.com>
2021-04-15 13:24:02 -0500

    Update the score bar styling
    Also remove the score bar from the individual hole page.

--- src/components/ScoreBar.vue
+++ src/components/ScoreBar.vue
@@ -1,20 +1,30 @@
 <template>
   <router-link :to="{ name: 'leaderboard', params: { tournamentId } }" class="block sticky top-0">
-    <div class="bg-blue-grey-100">
+    <div class="shadow border-b border-grey-400 bg-white">
       <div class="flex align-center justify-between px-2 pb-1">
-        <div class="w-1/2 flex items-center text-blue-800">
-          <div class="mr-4 text-5xl font-semibold">
+        <div class="w-1/2 flex items-center">
+          <div class="mr-3 text-6xl leading-none tracking-tighter font-semibold text-blue-800">
             {{ getScore('Blue') }}
           </div>
-          <div class="text-xl pt-1">
-            {{ getCaptain('Blue').lastName }}
+          <div class="text-xl pt-2">
+            <div class="uppercase text-grey-600 text-sm -mb-1 font-raleway">
+              Team
+            </div>
+            <div>
+              {{ getCaptain('Blue').lastName || 'Blue' }}
+            </div>
           </div>
         </div>
-        <div class="w-1/2 flex items-center justify-end text-red-800">
-          <div class="mr-4 pt-1 text-xl">
-            {{ getCaptain('Red').lastName }}
+        <div class="w-1/2 flex items-center justify-end">
+          <div class="mr-3 pt-2 text-xl">
+            <div class="uppercase text-grey-600 text-sm -mb-1 font-raleway">
+              Team
+            </div>
+            <div>
+              {{ getCaptain('Red').lastName || 'Red' }}
+            </div>
           </div>
-          <div class="text-5xl font-semibold">
+          <div class="text-6xl leading-tight tracking-tighter font-semibold text-red-800">
             {{ getScore('Red') }}
           </div>
         </div>
--- src/views/Hole.vue
+++ src/views/Hole.vue
@@ -1,7 +1,6 @@
 <template>
   <base-page>
-    <score-bar :tournament-id="tournamentId" />
-    <div class="sticky px-2 py-4 border-b border-grey-400 bg-white shadow" style="top: 66.5px">
+    <div class="sticky top-0 px-2 py-4 border-b border-grey-400 bg-white shadow">
       <match-summary v-if="match.id != null" v-bind="match" class="mb-4" />
       <div class="flex items-center justify-center text-grey-700">
         <div class="flex items-center mr-10">
@@ -39,12 +38,11 @@
 import BaseButton from '@/components/buttons/BaseButton';
 import BasePage from '@/components/layout/BasePage';
 import MatchSummary from '@/components/MatchSummary';
-import ScoreBar from '@/components/ScoreBar';
 import ScoreSlider from '@/components/ScoreSlider';
 import axios from '@/lib/axios';
 
 export default {
-  components: { BasePage, BaseButton, MatchSummary, ScoreBar, ScoreSlider },
+  components: { BasePage, BaseButton, MatchSummary, ScoreSlider },
 
   props: {
     tournamentId: {

bd3a134a473138be5fd77fd5e9735c652e16e857
Travis Bale <travis.bale@gmail.com>
2021-04-16 16:09:17 -0500

    Change the floating action button to a button

--- src/components/buttons/FloatingActionButton.vue
+++ src/components/buttons/FloatingActionButton.vue
@@ -2,10 +2,10 @@
   <div>
     <!-- Add some padding to the bottom of the page so the button doesn't completely osbscure the content -->
     <div class="mb-8" />
-    <div class="p-4 rounded-full bg-deep-purple-800 hover:bg-deep-purple-900 shadow-lg fixed bottom-0 right-0 m-4 text-white">
-      <add-icon v-if="action === 'add'" class="overflow-hidden text-white" v-on="$listeners" />
-      <edit-icon v-if="action === 'edit'" class="overflow-hidden text-white" v-on="$listeners" />
-    </div>
+    <button type="button" class="p-4 rounded-full bg-deep-purple-800 hover:bg-deep-purple-900 shadow-lg fixed bottom-0 right-0 m-4 text-white" v-on="$listeners">
+      <add-icon v-if="action === 'add'" class="overflow-hidden text-white" />
+      <edit-icon v-if="action === 'edit'" class="overflow-hidden text-white" />
+    </button>
   </div>
 </template>
 

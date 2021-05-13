314f947a779472363082fbbc7f7c6237ddf08768
Travis Bale <travis.bale@gmail.com>
2021-05-12 23:40:50 -0500

    Add the inline-block style to BaseBadge
    This class prevents the width from automatically expanding.

--- src/components/BaseBadge.vue
+++ src/components/BaseBadge.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="uppercase tracking-wider px-2 rounded-full text-xs font-semibold shadow">
+  <div class="inline-block uppercase tracking-wider px-2 rounded-full text-xs font-semibold shadow">
     <slot />
   </div>
 </template>

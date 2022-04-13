d77c09b93214fd124875b3ede07500cf09483403
Travis Bale <travis.bale@gmail.com>
2022-04-13 18:36:18 -0500

    Change font size for 'AS' text

--- src/components/MatchDetails.vue
+++ src/components/MatchDetails.vue
@@ -13,7 +13,7 @@
       </span>
       <span class="text-6xl">{{ statusText.substring(statusText.indexOf('&') + 2) }}</span>
     </div>
-    <div v-else class="text-4xl">
+    <div v-else class="text-5xl">
       {{ statusText }}
     </div>
   </div>
